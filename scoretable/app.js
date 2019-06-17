b25045eb117f0d22d98065676eddf28c881946a5
Adam Simon <adamosimoni@gmail.com>
2019-06-17 16:52:50 +0200

    tabella megjelenítése

--- scoretable/app.js
+++ scoretable/app.js
@@ -2,50 +2,50 @@
 // * nincs benne az elválasztó -> undefined
 // * van benne elválasztó -> { leftPart: "bal oldali rész", rightPart: "jobb oldali rész" }
 function separateStr(inputStr, separatorChar, fromLeft) {
-  var separatorIndex = fromLeft ?
-    inputStr.indexOf(separatorChar) :
-    inputStr.lastIndexOf(separatorChar);
+    var separatorIndex = fromLeft ?
+        inputStr.indexOf(separatorChar) :
+        inputStr.lastIndexOf(separatorChar);
 
-  if (separatorIndex < 0)
-    return;
+    if (separatorIndex < 0)
+        return;
 
-  return {
-    leftPart: inputStr.substr(0, separatorIndex),
-    rightPart: inputStr.substr(separatorIndex + 1)
-  }
+    return {
+        leftPart: inputStr.substr(0, separatorIndex),
+        rightPart: inputStr.substr(separatorIndex + 1)
+    }
 }
 
 // "Club Brugge-Borussia Dortmund 0:1"
 // ->
 // { homeTeam: "Club Brugge", awayTeam: "Borussia Dortmund", homeGoals: 0, awayGoals: 1 }
 function parseMatch(input) {
-  var matchParts = separateStr(input, " ", false);
-  if (typeof matchParts === 'undefined')
-    return;
+    var matchParts = separateStr(input, " ", false);
+    if (typeof matchParts === 'undefined')
+        return;
 
-  var resultParts = separateStr(matchParts.rightPart, ":", true);
-  if (typeof resultParts === 'undefined')
-    return;
+    var resultParts = separateStr(matchParts.rightPart, ":", true);
+    if (typeof resultParts === 'undefined')
+        return;
 
-  var homeGoals = parseInt(resultParts.leftPart);
-  var awayGoals = parseInt(resultParts.rightPart);
+    var homeGoals = parseInt(resultParts.leftPart);
+    var awayGoals = parseInt(resultParts.rightPart);
 
-  if (isNaN(homeGoals) || isNaN(awayGoals))
-    return;
+    if (isNaN(homeGoals) || isNaN(awayGoals))
+        return;
 
-  var teamParts = separateStr(matchParts.leftPart, "-", true);
-  if (typeof teamParts === 'undefined')
-    return;
+    var teamParts = separateStr(matchParts.leftPart, "-", true);
+    if (typeof teamParts === 'undefined')
+        return;
 
-  if (teamParts.leftPart.length === 0 || teamParts.rightPart.length === 0)
-    return;
+    if (teamParts.leftPart.length === 0 || teamParts.rightPart.length === 0)
+        return;
 
-  return {
-    homeTeam: teamParts.leftPart,
-    awayTeam: teamParts.rightPart,
-    homeGoals: homeGoals,
-    awayGoals: awayGoals
-  };
+    return {
+        homeTeam: teamParts.leftPart,
+        awayTeam: teamParts.rightPart,
+        homeGoals: homeGoals,
+        awayGoals: awayGoals
+    };
 }
 
 // visszatérési érték:
@@ -54,35 +54,81 @@ function parseMatch(input) {
 //   { homeTeam: 'Monaco', awayTeam: 'Atletico Madrid', homeGoals: 1, awayGoals: 2 },
 // ]
 function parseMatches(value) {
-  var result = [];
+    var result = [];
 
-  var lines = value.split("\n");
-  for (var i = 0; i < lines.length; i++) {
-    var line = lines[i].trim();
+    var lines = value.split("\n");
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i].trim();
 
-    if (line.length === 0)
-      continue;
+        if (line.length === 0)
+            continue;
 
-    var match = parseMatch(line);
-    if (typeof match === "undefined") {
-      alert('A(z) ' + (i + 1) + ". meccs hibás!");
-      return;
+        var match = parseMatch(line);
+        if (typeof match === "undefined") {
+            alert('A(z) ' + (i + 1) + ". meccs hibás!");
+            return;
+        }
+
+        result.push(match);
     }
 
-    result.push(match);
-  }
+    return result;
+}
 
-  return result;
+function displayTable(table) {
+    var tableContainer = document.getElementById("table-container");
+    tableContainer.innerHTML = "";
+
+    var tableElement = document.createElement("table");
+
+    // táblázat header
+    var tableHead = document.createElement("thead");
+
+    var tableRow = document.createElement("tr");
+
+    var cellTexts = ["#", "Team Name", "P", "W", "D", "L", "F", "A", "GD", "Pts"];
+    for (var i = 0; i < cellTexts.length; i++) {
+        var tableCell = document.createElement("th");
+        tableCell.innerText = cellTexts[i];
+        tableRow.appendChild(tableCell);
+    }
+
+    tableHead.appendChild(tableRow);
+
+    tableElement.appendChild(tableHead);
+
+    // táblázat tartalom
+    var tableBody = document.createElement("tbody");
+
+    for (var i = 0; i < table.length; i++) {
+        tableRow = document.createElement("tr");
+
+        var teamStat = table[i];
+        var cellTexts = [i + 1, teamStat.teamName, teamStat.played, teamStat.wins, teamStat.draws, teamStat.losts, teamStat.goalsFor, teamStat.goalsAgainst, teamStat.goalsDiff, teamStat.points];
+
+        for (var j = 0; j < cellTexts.length; j++) {
+            var tableCell = document.createElement("td");
+            tableCell.innerText = cellTexts[j];
+            tableRow.appendChild(tableCell);
+        }
+            
+        tableBody.appendChild(tableRow);
+    }
+
+    tableElement.appendChild(tableBody);
+
+    // A táblázatot a memóriában építjük, és csak a legvégén adjuk hozzá a DOM-hoz.
+    tableContainer.appendChild(tableElement);
 }
 
 function loadTable() {
-  var inputText = document.getElementById('inputText').value;
+    var inputText = document.getElementById('input-text').value;
 
-  var matches = parseMatches(inputText);
-  if (typeof matches === 'undefined')
-    return;
+    var matches = parseMatches(inputText);
+    if (typeof matches === 'undefined')
+        return;
 
-  var table = calcTable(matches);
+    var table = calcTable(matches);
 
-  console.log(table);
+    displayTable(table);
 }
--- scoretable/calc.js
+++ scoretable/calc.js
@@ -1,70 +1,72 @@
 function getTeamStat(stats, team) {
-  var stat = stats[team];
-  if (typeof stat === 'undefined')
-    stats[team] = stat = { played: 0, wins: 0, draws: 0, losts: 0, goalsFor: 0, goalsAgainst: 0, goalsDiff: 0, points: 0 };
-  return stat;
+    var stat = stats[team];
+    if (typeof stat === 'undefined')
+        stats[team] = stat = { played: 0, wins: 0, draws: 0, losts: 0, goalsFor: 0, goalsAgainst: 0, goalsDiff: 0, points: 0 };
+    return stat;
 }
 
 function calcTable(matches) {
-  // 1. feldolgozzuk a meccseredményeket -> kiszámoljuk az egyes csapatok statisztikáit
-  var stats = {};
+    // 1. feldolgozzuk a meccseredményeket -> kiszámoljuk az egyes csapatok statisztikáit
+    var stats = {};
 
-  for (var i = 0; i < matches.length; i++) {
-    var match = matches[i];
+    for (var i = 0; i < matches.length; i++) {
+        var match = matches[i];
 
-    //var result = splitResult(match.result);
+        //var result = splitResult(match.result);
 
-    var homeTeamStat = getTeamStat(stats, match.homeTeam);
-    var awayTeamStat = getTeamStat(stats, match.awayTeam);
+        var homeTeamStat = getTeamStat(stats, match.homeTeam);
+        var awayTeamStat = getTeamStat(stats, match.awayTeam);
 
-    homeTeamStat.played++;
-    awayTeamStat.played++;
+        homeTeamStat.played++;
+        awayTeamStat.played++;
 
-    if (match.homeGoals > match.awayGoals) {
-      homeTeamStat.wins++;
-      homeTeamStat.points += 3;
-      awayTeamStat.losts++;
+        if (match.homeGoals > match.awayGoals) {
+            homeTeamStat.wins++;
+            homeTeamStat.points += 3;
+            awayTeamStat.losts++;
+        }
+        else if (match.homeGoals < match.awayGoals) {
+            homeTeamStat.losts++;
+            awayTeamStat.wins++;
+            awayTeamStat.points += 3;
+        }
+        else {
+            homeTeamStat.draws++;
+            homeTeamStat.points++;
+            awayTeamStat.draws++;
+            awayTeamStat.points++;
+        }
+
+        homeTeamStat.goalsFor += match.homeGoals;
+        homeTeamStat.goalsAgainst += match.awayGoals;
+        homeTeamStat.goalsDiff += match.homeGoals - match.awayGoals;
+
+        awayTeamStat.goalsFor += match.awayGoals;
+        awayTeamStat.goalsAgainst += match.homeGoals;
+        awayTeamStat.goalsDiff += match.awayGoals - match.homeGoals;
     }
-    else if (match.homeGoals < match.awayGoals) {
-      homeTeamStat.losts++;
-      awayTeamStat.wins++;
-      awayTeamStat.points += 3;
+
+    // 2. a statisztika objektumot tömbbé (listává) alakítjuk
+    var match = [];
+    for (var team in stats) {
+        if (stats.hasOwnProperty(team)) {
+            var stat = stats[team];
+            stat.teamName = team;
+            match.push(stat);
+        }
     }
-    else {
-      homeTeamStat.draws++;
-      homeTeamStat.points++;
-      awayTeamStat.draws++;
-      awayTeamStat.points++;
-    }
 
-    homeTeamStat.goalsFor += match.homeGoals;
-    homeTeamStat.goalsAgainst += match.awayGoals;
-    homeTeamStat.goalsDiff += match.homeGoals - match.awayGoals;
+    // 3. lerendezzük a listát pontszám szerint
+    match.sort(function (a, b) {
+        if (a.points < b.points)
+            return 1;
+        else if (a.points > b.points)
+            return -1;
+        else
+            return 0;
+    });
 
-    awayTeamStat.goalsFor += match.awayGoals;
-    awayTeamStat.goalsAgainst += match.homeGoals;
-    awayTeamStat.goalsDiff += match.awayGoals - match.homeGoals;
-  }
+    // TODO: holtverseny kezelése
 
-  // 2. a statisztika objektumot tömbbé (listává) alakítjuk
-  var match = [];
-  for (var team in stats) {
-    if (stats.hasOwnProperty(team)) {
-      var stat = stats[team];
-      stat.teamName = team;
-      match.push(stat);
-    }
-  }
-
-  // 3. lerendezzük a listát pontszám szerint
-  match.sort(function (a, b) {
-    if (a.points < b.points)
-      return 1;
-    else if (a.points > b.points)
-      return -1;
-    else
-      return 0;
-  });
-
-  return match;
+    return match;
 }
